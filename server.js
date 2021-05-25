f49e75fa098dc77be92356a9dcbd2ed13718995d
anass99t <anass.taher@gmail.com>
2021-05-25 17:17:37 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,241 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const db = mysql.createConnection({
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: process.env.db_password,
+  database: process.env.db_db,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('MySql is connected');
+  }
+});
+
+// main variables
+var loggedIn = false;
+var message = null;
+var messages = [];
+// var name = null
+app.set('view-engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Validate cookies
+function validateCookie(req, res, next) {
+  const { cookies } = req;
+  if ('sessionToken' in cookies) {
+    loggedIn = true;
+    next();
+  } else {
+    loggedIn = false;
+    next();
+  }
+}
+
+app.get('/', validateCookie, (req, res) => {
+  res.render('home.ejs', {
+    loggedIn: loggedIn,
+    messages: messages,
+    name: req.cookies.name,
+    id: req.cookies.id,
+  });
+});
+
+app.get('/', validateCookie, (req, res) => {
+  res.render('home.ejs', {
+    loggedIn: loggedIn,
+    messages: messages,
+    name: req.cookies.name,
+    id: req.cookies.id,
+  });
+});
+
+app.get('/home', validateCookie, (req, res) => {
+  res.render('home.ejs', { loggedIn: loggedIn, messages: messages });
+});
+
+app.get('/login', validateCookie, (req, res) => {
+  if (!loggedIn) res.render('login.ejs', { loggedIn: false, message: message });
+  else res.redirect('/');
+});
+app.get('/register', validateCookie, (req, res) => {
+  if (!loggedIn)
+    res.render('register.ejs', { loggedIn: false, message: message });
+  else res.redirect('/');
+});
+
+app.post('/login', validateCookie, (req, res) => {
+  if (loggedIn) res.redirect('/');
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.render('login.ejs', {
+        loggedIn: false,
+        message: 'Email and password required',
+      });
+    }
+    db.query(
+      'SELECT * FROM users WHERE email = ?',
+      [email],
+      async (err, result) => {
+        // console.log(result);
+
+        if (
+          !result ||
+          !result.length > 0 ||
+          !result[0]['email'] ||
+          !(await bcrypt.compare(password, result[0].password))
+        ) {
+          res.render('login.ejs', {
+            loggedIn: false,
+            message: 'Email or password incorrect',
+          });
+        } else {
+          const id = result[0]['id'];
+          const name = result[0]['name'];
+          const token = jwt.sign({ id }, process.env.JWT_TOKEN, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+          });
+          const cookiesOptions = {
+            expires: new Date(
+              Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 3600000
+            ),
+            httpOnly: true,
+          };
+
+          res.cookie('sessionToken', token, cookiesOptions);
+          res.cookie('name', name, cookiesOptions);
+          res.cookie('email', email, cookiesOptions);
+          res.cookie('id', id, cookiesOptions);
+
+          loggedIn = true;
+          message = null;
+          res.redirect('/');
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/register', validateCookie, (req, res) => {
+  if (!loggedIn)
+    res.render('register.ejs', { loggedIn: loggedIn, message: message });
+  else res.redirect('/');
+});
+
+app.post('/register', validateCookie, async (req, res) => {
+  if (loggedIn) {
+    return res.redirect('/');
+  }
+  const { name, email, password, conf_password } = req.body;
+
+  //Verify if email already exist and passwords match
+  db.query(
+    'SELECT email from users WHERE email = ?',
+    [email],
+    async (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+      if (result.length > 0) {
+        return res.render('register.ejs', {
+          message: 'Email already exists',
+          loggedIn: false,
+        });
+      } else if (password !== conf_password) {
+        return res.render('register.ejs', {
+          message: "Passwords don't match",
+          loggedIn: false,
+        });
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        db.query(
+          'INSERT INTO users SET ?',
+          { name: name, email: email, password: hashedPassword },
+          (err, resu) => {
+            if (err) {
+              console.log(err);
+              return res.render('register.ejs', {
+                message: "Couldn't create the user, please try again",
+                loggin: false,
+              });
+            }
+          }
+        );
+        return res.redirect('/login');
+      }
+    }
+  );
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('sessionToken', '', { maxAge: 0 });
+  res.cookie('name', '', { maxAge: 0 });
+  res.cookie('email', '', { maxAge: 0 });
+
+  loggedIn = false;
+  message = null;
+  res.redirect('/');
+});
+
+app.get('/loadMessages', (req, res) => {
+  db.query(
+    'SELECT name, message FROM users , messages WHERE users.id = messages.user_id ORDER BY messages.time ASC',
+    (err, result) => {
+      if (err) console.log(err);
+      res.json(result);
+    }
+  );
+});
+
+app.post('/saveMessage', (req, res) => {
+  console.dir('saving :'+ JSON.stringify(req.body))
+  var data = JSON.stringify(req.body);
+  const NewMessage = JSON.parse(data);
+  console.log('newMessage :' + NewMessage.message);
+  db.query(
+    'INSERT INTO messages SET ?',
+    { message: NewMessage.message, user_id: NewMessage.user_id },
+    (err, result) => {
+      if (err) console.log(err);
+      res.json(result);
+    }
+  );
+});
+const server = app.listen(4040);
+console.log('Server is up on port 4040');
+
+const webSocket = require('ws');
+const SocketServer = require('ws').Server;
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('[server] a client was connected ');
+  ws.on('close', () => {
+    console.log('[server] client desconnected ');
+  });
+  ws.on('message', (message) => {
+    console.log('[server] receiced message %s', message);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === webSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
---
+++ socket.sql
@@ -0,0 +1,29 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ socket /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE socket;
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE `messages` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `message` varchar(255) DEFAULT NULL COMMENT 'message',
+  `user_id` int NOT NULL COMMENT 'foreign key',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `name` varchar(255) DEFAULT NULL COMMENT 'user name',
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+
+INSERT INTO messages(id,message,user_id,time) VALUES(63,'hello',6,'2021-05-25 17:10:10'),(64,'hey',5,'2021-05-25 17:10:14'),(65,'how are you',5,'2021-05-25 17:10:17'),(66,'I m good',6,'2021-05-25 17:10:22'),(67,'thank you',6,'2021-05-25 17:10:25'),(68,'goo bye',5,'2021-05-25 17:10:33'),(69,'g',5,'2021-05-25 17:10:36'),(70,'g',5,'2021-05-25 17:10:36'),(71,'g',5,'2021-05-25 17:10:37'),(72,'g',5,'2021-05-25 17:10:37'),(73,'g',5,'2021-05-25 17:10:37'),(74,'g',5,'2021-05-25 17:10:37');
+INSERT INTO users(id,name,email,password) VALUES(5,'ANASS OULED BEN TAHAR','anass.taher@gmail.com','$2b$10$iW9JcrYdq2wCygBlnSd7G.bOLCuX7kIB1uzGd5glqaneHqhF0H6LK'),(6,'obito','anass.taher1@gmail.com','$2b$10$XfVHxgNQXS3.GD4jeK0lb.p64dtlkJhs8xpOjydyKQeY4Zl29DLL2');
